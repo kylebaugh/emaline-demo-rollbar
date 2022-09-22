c0cc7b65532bb4d74e533e272e18a3f10bb8ba30
Kyle Baugh <kylebaugh1@gmail.com>
2022-09-22 13:17:22 -0600

    test 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const cors = require('cors')
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
 // include and initialize the rollbar library with your access token
